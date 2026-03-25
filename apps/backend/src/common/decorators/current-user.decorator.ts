e564d339ec0843b121c8fb15e3a61a147a092551
Vihanga Kumarasiri COR EKO LOITS <VihangaKu@lolctech.com>
2026-03-25 15:54:49 +0530

    Initial Test Comment

--- apps/backend/src/common/decorators/current-user.decorator.ts
+++ apps/backend/src/common/decorators/current-user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
-
+//Test Comment
 export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Request>();
